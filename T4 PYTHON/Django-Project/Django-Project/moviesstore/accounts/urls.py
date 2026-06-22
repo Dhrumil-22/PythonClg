28edc830292ee7e728fac00278067898001fd8e8
Dhrumil <dhrumilvaghela22@gmail.com>
2026-06-22 10:06:44 +0530

    account views working

--- T4 PYTHON/Django-Project/Django-Project/moviesstore/accounts/urls.py
+++ T4 PYTHON/Django-Project/Django-Project/moviesstore/accounts/urls.py
@@ -1,29 +1,8 @@
-"""
-URL configuration for moviesstore project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/6.0/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
-from django.contrib import admin
-from django.urls import path,include
-from home import views
-from movies import views as MV
-from django.conf.urls.static import static
-from django.conf import settings
-
+# offline ma signup ,signin , and logout no code lakho aema logout fuc nu name logout nai aapvanu km ke ae name alredy used thayelu che
+from django.urls import path
+from . import views
 
 urlpatterns = [
-    path('accounts/')
+    path('signup',views.signup,name='signup')
 ]
-urlpatterns += static(settings.MEDIA_URL, 
-                      document_root = settings.MEDIA_ROOT)
+
