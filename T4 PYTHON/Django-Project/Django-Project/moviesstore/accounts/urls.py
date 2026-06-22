2e24357baca655d2dd393971dd44a1968881915e
Dhrumil <dhrumilvaghela22@gmail.com>
2026-06-22 09:59:56 +0530

    account app working

--- T4 PYTHON/Django-Project/Django-Project/moviesstore/accounts/urls.py
+++ T4 PYTHON/Django-Project/Django-Project/moviesstore/accounts/urls.py
@@ -23,12 +23,7 @@ from django.conf import settings
 
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('',views.home,name='home'),
-    path('about/',views.about,name='about'),
-    path('movies/',MV.index, name = 'index'),   
-    path('movies/<int:id>',MV.show,name='show'),
-    path('accounts/',include('accounts.urls'))
+    path('accounts/')
 ]
 urlpatterns += static(settings.MEDIA_URL, 
                       document_root = settings.MEDIA_ROOT)
