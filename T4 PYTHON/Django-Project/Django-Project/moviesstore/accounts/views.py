b67a8c8d5a364ff3ef290876ada99798025e0425
Dhrumil <dhrumilvaghela22@gmail.com>
2026-06-23 12:30:43 +0530

    account signup

--- T4 PYTHON/Django-Project/Django-Project/moviesstore/accounts/views.py
+++ T4 PYTHON/Django-Project/Django-Project/moviesstore/accounts/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render,redirect
 from django.contrib.auth.forms import UserCreationForm
-from django.contrib.auth import authenticate,login as auth_login
+from django.contrib.auth import authenticate,login as auth_login,logout as auth_logout
 
 
 # Create your views here.
@@ -22,17 +22,21 @@ def signup(request):
 def login(request):
     template_data = {'title':'login'}
     if request.method=='GET':
-        return render(request, 'signup.html', {'template_data':template_data})
+        return render(request, 'login.html', {'template_data':template_data})
     elif request.method=='POST':
         user = authenticate(request,
                             username = request.POST['username'],
-                            password = request.Post['password'])
+                            password = request.POST['password'])
         if user is None:
             template_data['error']='The user or password is incorrect'
-            return render(request, 'signup.html', {'template_data':template_data})
+            return render(request, 'login.html', {'template_data':template_data})
         else:
             auth_login(request,user)
-            return redirect('home')       
+            return redirect('home')   
+        
+def logout(request):
+    auth_logout(request)
+    return redirect('home')    
         
         
             
